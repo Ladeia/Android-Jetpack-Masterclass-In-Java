9cb05b0d648abca40a15086ef716fc47b33a166c
Antonio Ladeia <contato@antonioladeia.com>
2020-12-15 09:30:20 -0300

    redo the project from 0

---
+++ app/src/main/java/com/antonioladeia/dogsapp/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.antonioladeia.dogsapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.antonioladeia.dogsapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
